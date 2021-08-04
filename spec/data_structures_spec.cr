9a2461297c7620970e4c2d4392337f3f08bc5a4c
Fabien Schurter <fabien@fabschurt.com>
2021-08-04 16:04:14 +0200

    chore: Reorganize the codebase a bit

--- spec/data_structures_spec.cr
+++ spec/data_structures_spec.cr
@@ -1,5 +1,59 @@
 require "spec"
-require "data_structures"
+
+enum BookType
+  COMICS
+  MANGA
+  COLLECTOR
+  NOVEL
+end
+
+struct Name
+  @value : String
+
+  def initialize(value : String)
+    if value == ""
+      raise ArgumentError.new("A name cannot be empty.")
+    end
+
+    @value = value
+  end
+
+  def to_s(io : IO)
+    io << @value
+  end
+end
+
+struct Author
+  @first_name : Name
+  @last_name : Name
+
+  def initialize(
+    @first_name : Name,
+    @last_name : Name,
+  ) end
+
+  def to_s(io : IO)
+    io << "#{@first_name} #{@last_name}"
+  end
+end
+
+struct Book
+  getter type, title, authors
+
+  @type : BookType
+  @title: Name
+  @authors : Array(Author)
+
+  def initialize(
+    @type : BookType,
+    @title : Name,
+    @authors : Array(Author),
+  ) end
+
+  def to_s(io : IO)
+    io << "#{@title} (#{@authors.join(", ")})"
+  end
+end
 
 def create_book
   Book.new(
@@ -26,9 +80,7 @@ describe Name do
   end
 
   it "cannot be empty" do
-    expect_raises(ArgumentError) do
-      Name.new("")
-    end
+    expect_raises(ArgumentError) { Name.new("") }
   end
 end
 
